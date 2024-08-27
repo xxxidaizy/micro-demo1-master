58b8024af8c685a9059a75ce99a7107d2ba72baa
zhangqi <zhangqi@neolix.cn>
2024-08-27 20:29:00 +0800

    stash

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { VideoCameraOutlined } from "@ant-design/icons"
+import { NavLink } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
+const { Header, Content, Footer } = Layout;
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Layout>
+      <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        <div className="demo-logo"><h1 style={{ color: '#fff' }}>名称</h1></div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ flex: 1 }}
+          items={[
+            {
+              key: "1",
+              icon: <VideoCameraOutlined />,
+              label: <NavLink to="/react-micro1">react 1 应用</NavLink>
+            },{
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: <NavLink to="/">react 2 应用</NavLink>
+            }
+          ]}
+        />
+      </Header >
+      <Content
+        style={{ height: 'calc(100vh - 128px)', backgroundColor: '#b4efe1' }}
+        id="container"
+      >
+        <h1>主应用显示容器</h1 >
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </Content >
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer >
+    </Layout >
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { registerMicroApps, start } from "qiankun";
+import { ConfigProvider } from 'antd';
+import zhCN from "antd/locale/zh_CN";
+
+registerMicroApps([
+  {
+    name: "react-micro-application", //子应用的名称，必须唯-。
+    entry: "http://localhost:7788",//子应用项目本地运行地址
+    container: "#container", // 子应用的容器(子应用嵌入到主项目id为container的容器)
+    activeRule: "/react-micro1",// 子应用激活时的路由规则(子应用路由)
+    props: { //向子应用传参
+      state: false,
+      userId: "userId",
+      token: "18145EWDFDEdsefEFFfdDFf145454"
+    }
+  }
+]);
+start();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <ConfigProvider locale={zhCN}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ConfigProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
